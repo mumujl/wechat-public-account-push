981635e82e6ce32bdefd1954fd0d7b4f91f4f9d1
mumujl <101856873+mumujl@users.noreply.github.com>
2022-10-01 12:07:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'os5wX6G-i5fEGVduPfbox17S4tko',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ftXkG4EdGjpn4JzVvLh1EnBZYRaaSjK0Ypq58sy811A',
+      useTemplateId: 'C14vusLGuBzxxrbojRfsXoyT9_JhIfHNt3KEFfVj13g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
